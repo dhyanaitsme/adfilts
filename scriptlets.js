b7c514bf61cb80a4e00854acae6e204d7bb3112b
dhyanaitsme <122237810+dhyanaitsme@users.noreply.github.com>
2023-01-10 16:29:31 +0100

    Update scriptlets.js

--- scriptlets.js
+++ scriptlets.js
@@ -220,7 +220,7 @@
 /// alias orr.js
 (function() {
     "use strict";
-    if (window.location.href.includes("/www.reddit.com/r/popular/?geo_filter=US/") && !window.location.href.includes("/www.reddit.com/gallery/") && !window.location.href.includes("/www.reddit.com/poll/")) {
+    if (window.location.href.includes("/www.reddit.com/") && !window.location.href.includes("/www.reddit.com/r/popular/?geo_filter=US/") && !window.location.href.includes("/www.reddit.com/gallery/") && !window.location.href.includes("/www.reddit.com/poll/")) {
         window.location.replace(window.location.toString().replace("/www.reddit.com/", "/www.reddit.com/r/popular/?geo_filter=US/"));
     }
 })();
